74e3b2ffdb0d6f031757849a01d0a85229e66df1
ljy <jinyyu@hotmail.com>
2018-11-15 00:22:56 +0800

    add common line parser

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 #[macro_use]
 extern crate log;
 extern crate env_logger;
+extern crate argparse;
 
 use std::io::Write;
 use env_logger::Builder;
 
-
 fn main() {
+    let mut config = "/etc/filedump/config.yaml".to_string();
+    {
+        let mut ap = argparse::ArgumentParser::new();
+        ap.set_description("file dump");
+        ap.refer(&mut config)
+            .add_option(&["-c", "--config"], argparse::Store,
+                        "config file path");
+        ap.parse_args_or_exit();
+    }
+
     Builder::from_default_env()
         .format(|buf, record| {
             writeln!(buf, "[{}] [{}:{}] {}", record.level(), record.file().unwrap(), record.line().unwrap(), record.args())
-        })
-        .init();
+        }).init();
 
-    debug!("hihihi")
+    debug!("config path = {}", config)
 }
