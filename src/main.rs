6a1bd673a9bfe0fbbc901f6005ab99e47a9421d9
ljy <jinyyu@hotmail.com>
2018-11-14 23:42:54 +0800

    add env_log

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+use std::io::Write;
+use env_logger::Builder;
+
+
 fn main() {
-    println!("Hello, world!");
+    Builder::from_default_env()
+        .format(|buf, record| {
+            writeln!(buf, "[{}] [{}:{}] {}", record.level(), record.file().unwrap(), record.line().unwrap(), record.args())
+        })
+        .init();
+
+    debug!("hihihi")
 }
