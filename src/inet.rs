11ae319e302fd2b78c14f8096c400d4c7e2e8357
ljy <ljy@zhsftech.club>
2018-11-20 11:04:32 +0800

    update

--- src/inet.rs
+++ src/inet.rs
@@ -15,11 +15,13 @@ extern "C" {
 pub fn ip_to_string(ip: u32) -> String {
     let mut array: [u8; 16] = [0; 16];
     let raw: *const u32 = &ip as *const u32;
+    let c_str;
     unsafe {
         inet_ntop(AF_INET as c_int, raw as *const c_char, array.as_mut_ptr() as *mut c_char, 16);
-        let c_str = CStr::from_bytes_with_nul_unchecked(&array);
-        return c_str.to_string_lossy().into_owned();
+        c_str = CStr::from_bytes_with_nul_unchecked(&array);
+
     }
+    return c_str.to_string_lossy().into_owned();
 }
 
 
